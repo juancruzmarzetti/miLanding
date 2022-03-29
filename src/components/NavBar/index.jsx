d2f7c7fdde3db60b8b1c7c525077f55d429d26e2
juancruzmarzetti <naujmarz@gmail.com>
2022-03-29 18:26:53 -0300

    ReactPub-MarzettiFalcone

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+
+export default function NavBarr(){
+
+    return(
+        <>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">React Pub</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Inicio</Nav.Link>
+                            <Nav.Link href="#link">Blog</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Facebook</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Instagram</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Twitter</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">WhatsApp</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+      </>
+    );
+}
